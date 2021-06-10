cc718b796b74715cb4f599b6b8a664bc5f8aa9ec
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-10 15:14:26 +0200

    add chapiter 1 base ; exercise 3

---
+++ base-exercises/03-prompt-if/script.js
@@ -0,0 +1,12 @@
+
+
+
+  var userCake = confirm ("Would you like some cake?");
+
+  if(userCake==true)
+  {
+    alert("Congratulations, here you are !");
+  }
+  else{
+    alert("More cake for me then :p !");
+  }
