b37e0bcbf77602c87d55e7a1f0562772c7ba2408
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-11 12:09:33 +0200

    add chapiter 2 colors : exercise 1

---
+++ colors-exercises/01-change-bcg-one/script.js
@@ -0,0 +1,76 @@
+
+  function changeBackground(bgColor) {
+    document.body.style.background = bgColor;
+  }
+
+  //Try out
+/*
+function changeBackgroundColor (){
+    var red = document.getElementById ("red").body.style.backgroundColor="red";
+    var green = document.getElementById ("green").body.style.backgroundColor="green";
+    var yellow = document.getElementById ("yellow").body.style.backgroundColor="yellow";
+    var blue = document.getElementById ("blue").body.style.backgroundColor="blue";
+}
+*/
+
+
+//Try out
+/*
+function changeBackgroundColor (newColor){
+    var red = document.getElementById ("red");
+    var green = document.getElementById ("green");
+    var yellow = document.getElementById ("yellow");
+    var blue = document.getElementById ("blue");
+    document.body.style.backgroundColor= newColor;
+
+}
+*/
+
+
+//Try out
+/*function myFunction(id){
+    document.getElementById(id);
+    document.body.style.backgroundColor= "red";
+    document.body.style.backgroundColor= "green";
+    document.body.style.backgroundColor= "yellow";
+    document.body.style.backgroundColor= "blue";
+}
+*/
+
+//Try out
+/* function myFunction(){
+    document.body.style.backgroundColor= id;
+*/
+
+
+
+//Try out
+/* function myFunction(){
+    document.body.style.backgroundColor= "red";
+    document.body.style.backgroundColor= "green";
+    document.body.style.backgroundColor= "yellow";
+    document.body.style.backgroundColor= "blue";
+}
+myFunction();
+*/
+
+
+
+//Try out
+/* function myFunction(){
+    document.body.style.backgroundColor= "red";
+}
+
+function myFunction(){
+    document.body.style.backgroundColor= "green";
+}
+
+function myFunction(){
+    document.body.style.backgroundColor= "yellow";
+}
+
+function myFunction(){
+    document.body.style.backgroundColor= "blue";
+}
+
+*/
