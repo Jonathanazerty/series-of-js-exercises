96facaf0dc779376f6cac5cfcc6770ef45888ff9
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-15 14:46:18 +0200

    add exercises 1 and 2 from chapiter 4

--- 04-maths-exercises/01-calculator-one/script.js
+++ 04-maths-exercises/01-calculator-one/script.js
@@ -1,30 +1,32 @@
-/* becode/javascript
- *
- * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
-    // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+    let operand1 = document.getElementById("op-one").value;
+    let operand2 = document.getElementById("op-two").value;
+    let addition = parseInt(operand1) + parseInt(operand2);
+    document.write(addition);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        let operand1 = document.getElementById("op-one").value;
+        let operand2 = document.getElementById("op-two").value;
+        let substraction = parseInt(operand1) - parseInt(operand2);
+        document.write(substraction);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        let operand1 = document.getElementById("op-one").value;
+        let operand2 = document.getElementById("op-two").value;
+        let multiplication = parseInt(operand1) * parseInt(operand2);
+        document.write(multiplication);
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+        let operand1 = document.getElementById("op-one").value;
+        let operand2 = document.getElementById("op-two").value;
+        let division = parseInt(operand1) / parseInt(operand2);
+        document.write(division);
     });
 })();
--- 04-maths-exercises/02-calculator-two/script.js
+++ 04-maths-exercises/02-calculator-two/script.js
@@ -1,24 +1,33 @@
-/* becode/javascript
- *
- * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
-    // to get the value of an input: document.getElementById("element-id").value
 
-    var performOperation = function(operation) {
-        // perform the operation
+    let operation = function(operation) {
+        let operand1 = document.getElementById("op-one").value;
+        let operand2 = document.getElementById("op-two").value;
+        let result;
+
+        switch (operation) {
+            case "addition":
+                result = parseInt(operand1) + parseInt(operand2);
+                break;
+            case "substraction":
+                result = parseInt(operand1) - parseInt(operand2);
+                break;
+            case "multiplication":
+                result = parseInt(operand1) * parseInt(operand2);
+                break;
+            case "division":
+                result = parseInt(operand1) / parseInt(operand2);
+                break;
+        }
+
+        document.write(result);
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
         $btn.addEventListener("click", function() {
-            performOperation($btn.id);
+            operation($btn.id);
         });
     });
 })();
