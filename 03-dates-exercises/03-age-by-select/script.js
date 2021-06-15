5686715328c948d9c99dc297ceb7f7e841036eb9
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-15 11:21:19 +0200

    add shared file and exercise 3 from chapiter 3

--- 03-dates-exercises/03-age-by-select/script.js
+++ 03-dates-exercises/03-age-by-select/script.js
@@ -1,6 +1,30 @@
 
+(function() {
 
-function CalculateAge () {
+    var btn = document.getElementById('run');
+      
+    btn.addEventListener('click', function(e){
+    
+        var dayElement = document.getElementById('dob-day'); 
+        var monthElement = document.getElementById('dob-month'); 
+        var yearElement = document.getElementById('dob-year'); 
+  
+  let birthday = new Date(yearElement.value, monthElement.value, dayElement.value);         
+    console.log(birthday);
+  
+  let ageDifMs = Date.now() - birthday.getTime();
+    console.log(ageDifMs);
+  let ageDate = new Date(ageDifMs);
+    console.log(ageDate);
+  let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    console.log(age);
+  
+     })
+  
+  })();
+
+/* 
+
     let userInput = document.getElementById("dob-day","dob-month", "dob-year").value;
     let dob = new Date(userInput);
     if(userInput==null || userInput== '') {
@@ -10,18 +34,19 @@ function CalculateAge () {
 
     else {
     //calculate month difference from current date in time  
-    var month_diff = Date.now() - dob.getTime();  
+    let month_diff = Date.now() - dob.getTime();  
       
     //convert the calculated difference in date format  
-    var age_dt = new Date(month_diff);   
+    let age_dt = new Date(month_diff);   
       
     //extract year from date      
-    var year = age_dt.getUTCFullYear();  
+    let year = age_dt.getUTCFullYear();  
       
     //now calculate the age of the user  
-    var age = Math.abs(year - 1970);  
+    let age = Math.abs(year - 1970);  
+
+    console.log(age);
 
     return document.getElementById("run").innerHTML = "You are" + age + " years old !";
     }
-}
-
+*/
