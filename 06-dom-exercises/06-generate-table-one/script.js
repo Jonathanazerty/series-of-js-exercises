ef935f22e22855aa426265e0017556d0b3ff253e
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-22 17:22:16 +0200

    busy with exercise 6 of chapiter 6

---
+++ 06-dom-exercises/06-generate-table-one/script.js
@@ -0,0 +1,15 @@
+(function() {
+    // TODO: inside id target
+    const body = document.querySelector("#target");
+    // TODO: create table
+    const table = document.createElement("table");
+    // TODO: create table body
+    const tableBody = document.createElement("tbody");
+
+    // TODO: make column and 10 rows
+    for (let i=0; i<10; i++) {
+        
+    }
+
+
+})();
