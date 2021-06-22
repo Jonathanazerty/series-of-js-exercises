6f0f702f5d8a3531c01d146ddccf884cf0bf022f
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-22 12:30:07 +0200

    add exercises chapiter 6 & begin of exercise 5

--- 06-dom-exercises/04-attr-create/script.js
+++ 06-dom-exercises/04-attr-create/script.js
@@ -1,16 +1,25 @@
-/* becode/javascript
- *
- * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
-    // your code here
+
+    // TODO: get the image 
+    const sourceImage = document.querySelector("#source");
+
+    const valueImage = sourceImage.getAttribute("data-image");
+    console.log(valueImage);
+
+
+    // TODO: create new element
+    const newImage = document.createElement("img");
+
+    newImage.src = valueImage;
+
+    // TODO: add new image on target
+    document.querySelector("#target").appendChild(newImage);
+    
+
+    // TODO: remove original element
+    sourceImage.remove();
+
 
 })();
