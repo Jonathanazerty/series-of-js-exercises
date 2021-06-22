ade96276395d08a78b75a0c7b1e6d319c8c7f3dd
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-22 14:43:23 +0200

    completed chapiter 6

--- 06-dom-exercises/05-hover-image/script.js
+++ 06-dom-exercises/05-hover-image/script.js
@@ -2,10 +2,20 @@
 
 (function() {
 
+    // TODO: make variables to id them
     const img = document.querySelector("img");
+
     const sourceImage = img.getAttribute("src");
+
     const hoverImage = img.getAttribute("data-hover");
 
+    // TODO: change image when hovering over image
+    img.addEventListener("mouseover", function( ){
+        img.src = hoverImage;
+    });
 
 
+    img.addEventListener("mouseout", function(){
+        img.src = sourceImage;
+    });
 })();
