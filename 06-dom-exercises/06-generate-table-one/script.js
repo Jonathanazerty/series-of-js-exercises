a3298ca3b7b1e517c32c1d17d93c16de4f9c842e
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-23 10:58:57 +0200

    finished exercise 6 of chapiter 6

--- 06-dom-exercises/06-generate-table-one/script.js
+++ 06-dom-exercises/06-generate-table-one/script.js
@@ -8,6 +8,29 @@
 
     // TODO: make column and 10 rows
     for (let i=0; i<10; i++) {
+        //TODO: create row
+        const tr = document.createElement("tr");
+        //TODO: create cell
+        const td = document.createElement("td");
+        //TODO: create text inside cell
+        const text = document.createTextNode("row");
+
+        // TODO: add everything to html
+        // TODO: add text
+        td.appendChild(text);
+
+        // TODO: add cell
+        tr.appendChild(td);
+
+        // TODO: add row
+        tableBody.appendChild(tr);
+
+        // TODO: add table body
+        table.appendChild(tableBody);
+
+        // TODO: add table
+        body.appendChild(table);
+
         
     }
 
