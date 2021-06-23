4ff05af2282745324b52b6b1c78d5a1ca7c014da
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-23 12:22:54 +0200

    finished exercise 1 of chapiter 8

--- 08-fetch-exercises/01-list-to-console/script.js
+++ 08-fetch-exercises/01-list-to-console/script.js
@@ -1,12 +1,18 @@
-/* becode/javascript
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+    
+    document.getElementById("run").addEventListener("click", function(){
+
+        async function heroesData()
+        {
+            let data = await fetch("http://127.0.0.1:5500/01-list-to-console/api.json");
+
+            let heroes = await data.json();
+            console.log(heroes);
+        }
+
+        heroesData();
+    });
+
 })();
