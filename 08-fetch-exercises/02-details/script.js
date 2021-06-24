251d4a64c0fd9ee300dc60061871ae057af657e5
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-24 13:28:49 +0200

    finished exercise 2 of chapiter 8

--- 08-fetch-exercises/02-details/script.js
+++ 08-fetch-exercises/02-details/script.js
@@ -1,12 +1,41 @@
-/* becode/javascript
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    // TODO: click 
+    document.getElementById("run").addEventListener("click", function(){
+
+        // TODO async method
+        async function heroesAllData()
+        {
+            let data = await fetch("http://127.0.0.1:5500/01-list-to-console/api.json");
+
+            let heroes = await data.json();
+            console.log(heroes);
+            console.log(Object.keys(heroes));
+            console.log(Object.values(heroes));
+            {
+
+            // TODO: id from the form and input
+            const heroesInput = document.querySelector("#hero-id").value - 1;
+            
+            // TODO: template
+            const template = document.getElementById("tpl-hero").content;
+            const copyTemplate = document.importNode(template, true);
+
+            const hEROES = heroes.heroes[heroesInput];
+            console.log(hEROES);
+
+            copyTemplate.querySelector(".name").innerHTML = hEROES.name;
+            copyTemplate.querySelector(".alter-ego").innerHTML = hEROES.alterEgo;
+            copyTemplate.querySelector(".powers"). innerHTML = hEROES.abilities;
+
+            document.querySelector("#target").appendChild(copyTemplate);
+            
+            }
+        }
+
+        heroesAllData();
+    });
+
 })();
